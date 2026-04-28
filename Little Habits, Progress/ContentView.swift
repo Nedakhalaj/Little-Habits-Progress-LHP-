856308b24352a6798140b1d2cb29946f4f4cd3aa
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-04-28 15:37:11 +0200

    Initial Commit

---
+++ Little Habits, Progress/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Habits, Progress
+//
+//  Created by neda khalajnejad on 2026-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Habits, Progress/Little_Habits__ProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Habits__ProgressApp.swift
+//  Little Habits, Progress
+//
+//  Created by neda khalajnejad on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct Little_Habits__ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
