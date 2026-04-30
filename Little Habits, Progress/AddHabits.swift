877ed757d0000bb86e51a79f515a3fbc38766de3
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-04-30 11:10:09 +0200

    Added Tabs

---
+++ Little Habits, Progress/AddHabits.swift
@@ -0,0 +1,18 @@
+//
+//  AddHabits].swift
+//  Little Habits, Progress
+//
+//  Created by neda khalajnejad on 2026-04-30.
+//
+import SwiftUI
+
+struct AddHabits: View {
+    var body: some View {
+        ZStack {
+            Color("AppBackground")
+                .ignoresSafeArea()
+            Text("Add Habits")
+        }
+        
+    }
+}
---
+++ Little Habits, Progress/Setting.swift
@@ -0,0 +1,20 @@
+//
+//  Setting.swift
+//  Little Habits, Progress
+//
+//  Created by neda khalajnejad on 2026-04-30.
+//
+
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        ZStack{
+            Color("AppBackground")
+                .ignoresSafeArea()
+            Text("Settings")
+        }
+      
+    }
+}
